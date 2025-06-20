d89ebb0d5e7e915b108a9287b9df868d2d655590
Leliel Trethowen <lelitu@proton.me>
2025-06-20 17:25:26 +1200

    Convert to NPM from Yarn
    Quickest solution to an issue with vscode plugin detecting playwright
    tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,5 +58,22 @@ export default defineConfig({
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
     },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
